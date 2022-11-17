56e9fd6b9b89fde7d5436079eebe12a0c46ec377
scenaristeur <scenaristeur@gmail.com>
2022-11-17 01:37:24 +0100

    contextmenu

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import VueIpfs from './plugins/vue-ipfs.js'
 import store from './store'
+import VueSimpleContextMenu from 'vue-simple-context-menu';
+import 'vue-simple-context-menu/dist/vue-simple-context-menu.css';
 
 // Load our IPFS plugin.
 const app = createApp(App)
+app.component('vue-simple-context-menu', VueSimpleContextMenu);
 app.use(store);
 app.use(VueIpfs, {store: store})
 app.mount('#app')
